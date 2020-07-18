ead6e01ad3dcd7df8f6dee45a870d06d47f3944a
Assane Thione Ba <assanetb@gmail.com>
2020-07-18 00:05:09 +0000

    Unit tests

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UserTest extends KernelTestCase
+{
+    private $user;
+    private $task;
+    private $createdAt;
+
+    public function setUp(): void
+    {
+        $this->user = new User();
+        $this->task = new Task();
+        $this->createdAt = new \DateTime();
+    }
+
+    public function testId()
+    {
+        self::assertEquals(null, $this->user->getId());
+    }
+
+    public function testUsername()
+    {
+        $this->user->setUsername('usertest');
+        self::assertEquals('usertest', $this->user->getUsername());
+    }
+
+    public function testPassword()
+    {
+        $this->user->setPassword('passer12345');
+        self::assertEquals('passer12345', $this->user->getPassword());
+    }
+
+    public function testEmail()
+    {
+        $this->user->setEmail('user@mail.com');
+        self::assertEquals('user@mail.com', $this->user->getEmail());
+    }
+
+    public function testGetSalt()
+    {
+        self::assertEquals(null, $this->user->getSalt());
+    }
+
+    public function testAuthor()
+    {
+        $this->user->setRoles(['ROLE_USER']);
+        self::assertEquals(array('0' => 'ROLE_USER'), $this->user->getRoles());
+    }
+
+    public function testEraseCredentials()
+    {
+        $user = $this->user;
+        $this->user->eraseCredentials();
+        self::assertEquals($user, $this->user);
+    }
+
+    public function testUserGetId()
+    {
+        self::assertEquals(null, $this->user->getId());
+    }
+
+    public function testGetTask():void
+    {
+        //test add method
+        $value = new Task();
+        $response = $this->user->addTask($value);
+
+        self::assertInstanceOf(User::class, $response);
+        self::assertCount(1, $this->user->getTasks());
+        self::assertTrue($this->user->getTasks()->contains($value));
+
+        //test remove method
+        $response = $this->user->removeTask($value);
+
+        self::assertInstanceOf(User::class, $response);
+        self::assertCount(0, $this->user->getTasks());
+        self::assertFalse($this->user->getTasks()->contains($value));
+    }
+}
